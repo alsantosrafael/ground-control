5f58bd9a78c059e62c99f8f3e98c418d8f5a80a5
Rafael Almeida <rafael.profeng@gmail.com>
2025-07-08 18:31:21 -0300

    feat: First commit

---
+++ src/main/kotlin/com/platform/groundcontrol/GroundControlApplication.kt
@@ -0,0 +1,11 @@
+package com.platform.groundcontrol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroundControlApplication
+
+fun main(args: Array<String>) {
+	runApplication<GroundControlApplication>(*args)
+}
---
+++ src/test/kotlin/com/platform/groundcontrol/GroundcontrolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.platform.groundcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroundcontrolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
